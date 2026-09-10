4bd01ee44c4079c8c71b5a704ca5322ef1c94fe5
Laboratorio Asu <laboratorioasu@unida.edu.py>
2026-09-10 20:52:51 -0300

    Lol

---
+++ Holamundo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+ using namespace std;
+ int main (){
+ 		//B
+ 		cout <<" *****" "\n";
+		cout <<"*" "\      " "*" "\n";
+		cout <<"*" "\t" "*" "\n";
+ 		cout <<"*" "\      " "*" "\n";
+ 		cout <<"******" "\n";
+ 	    cout <<"*" "\      " "*" "\n";
+		cout <<"*" "\t" "*" "\n";
+ 		cout <<"*" "\      " "*" "\n";
+ 		cout <<" ******" "\n";
+ 		return 0; 
+	 }
+	 
